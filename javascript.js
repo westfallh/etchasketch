e10dd32b7aa177cd531fc2b16f610721e274594f
Hayden Westfall <hwestfall04@gmail.com>
2025-01-07 13:14:35 -0800

    added grid and mouseover event

--- javascript.js
+++ javascript.js
@@ -1,13 +1,21 @@
-
-
 const grid = document.getElementById('grid');
-
-for(let i = 0; i < 32; i++) {
+grid.style.display = 'flex';
+grid.style.flexWrap = 'wrap';
+grid.style.justifyContent = 'center';
+grid.style.width = '100%';
+grid.style.height = '100vh';
+grid.style.gap = '2px';
+for(let i = 0; i < 256; i++) {
     const newDiv = document.createElement("div");
-    newDiv.style.width = "100px";
-    newDiv.style.height = "100px";
-    newDiv.style.background = "gray";
+    newDiv.style.width = "50px";
+    newDiv.style.height = "50px";
+    newDiv.style.background = "white";
     grid.appendChild(newDiv);
+    
+    newDiv.addEventListener('mouseover', function() {
+        newDiv.style.background = "black";
+    })
+    
 }
 
 
