e45426bba4a57435f4a900a1141187e660c72aa1
Hayden Westfall <hwestfall04@gmail.com>
2025-01-09 10:56:06 -0800

    Added way to clear grid, initial instructions done

--- javascript.js
+++ javascript.js
@@ -1,20 +1,27 @@
-let size = parseInt(prompt('Enter initial grid size'), 10);
+let size = 16;
 
 
 function createGrid(size) {
     const grid = document.getElementById('grid');
+    grid.innerHTML = '';
+
     grid.style.display = 'flex';
     grid.style.flexWrap = 'wrap';
     grid.style.justifyContent = 'center';
-    grid.style.width = '800px';
-    grid.style.height = '800px';
+    const containerWidth = 800;
+    const containerHeight = 800;
+    grid.style.width = `${containerWidth}px`;
+    grid.style.height = `${containerHeight}px`;
+
     grid.style.margin = 'auto';
     grid.style.border = '1px solid black';
 
+    
+    const squareSize = containerWidth / size;
     for(let i = 0; i < size * size; i++) {
         const newDiv = document.createElement("div");
-        newDiv.style.width = "50px";
-        newDiv.style.height = "50px";
+        newDiv.style.width = `${squareSize}px`;
+        newDiv.style.height = `${squareSize}px`
         newDiv.style.background = "white";
         grid.appendChild(newDiv);
         
@@ -28,10 +35,13 @@ createGrid(size);
 const gridReset = document.getElementById('gridreset');
 
 gridReset.addEventListener('click',  () => {
-     let size = parseInt(prompt('Enter grid size: 1-100. (e.g. 4 for a 4x4 grid)', 16), 10);
-        if (Number.isInteger(size) && size >= 1 && size <= 100){
-            createGrid(size)
-        } 
-})
+     let newSize = parseInt(prompt('Enter grid size: 1-100. (e.g. 4 for a 4x4 grid)', 16), 10);
+        if (Number.isInteger(size) && size >= 1 && size <= 99){
+            size = newSize;
+            createGrid(size);
+        } else {
+            alert('Invalid input! Please enter a number between 1 and 100.');
+        }
+});
 
 
